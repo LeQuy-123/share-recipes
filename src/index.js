9a11c5cac81b3b9cb579993537a4a57644191dd6
LeQuy-123 <Ledaosyquy@gmail.com>
2020-12-08 15:49:25 +0700

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename={window.location.pathname || ''}>
           <App />
         </BrowserRouter>
       </PersistGate>
